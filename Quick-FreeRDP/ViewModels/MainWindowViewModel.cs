1aa4a31f10eee3327720da0c4c8901197f89777c
Martin Parry <mparry96@live.co.uk>
2026-04-20 21:09:36 +0100

    Properties created

---
+++ Quick-FreeRDP/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using  System.Collections.Generic;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Quick_FreeRDP.ViewModels;
+
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string RdpName { get; set; } = string.Empty;
+    
+    public string IpAddress { get; set; } = string.Empty;
+
+    public int ResolutionWidth { get; set; }
+    
+    public int ResolutionHeight { get; set; }
+
+    public bool FullScreenBool { get; set; } = false;
+
+    public bool FloatBarBool { get; set; } = false;
+    
+    public List<string> RdpItems { get; set; } = new()
+    {
+        "Option 1",
+        "Option 2",
+        "Option 3"
+    };
+    
+    
+    
+}
